47b165e65599fd61fd6b954f88b8eef1f4b7bc27
Chris <crlang44@gmail.com>
2017-04-07 13:53:21 -0700

    Display error message when there is no internet connection.

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ let dictionary = new Dictionary();
 let httpRequest = new HttpRequest();
 //(Math.random() * (max - min) + min)
 let interval = (Math.random() * (10000 - 5000)) + 5000;
-
-let logRequests = false;
+let logRequests = localStorage.getItem("logRequests");
+let isOffline = false;
 
 function toggleLogRequests() {
     logRequests = !logRequests;
@@ -16,10 +16,16 @@ function getLogRequests() {
 
 window.setInterval(function () {
     if (navigator.onLine) {
+        isOffline = false;
         let url = dictionary.randomUrl;
         httpRequest.get(url);
         if (logRequests) {
             console.log(url);
         }
+    }else{
+        if(!isOffline){
+            console.log("No Internet Connection");
+        }
+        isOffline = true;
     }
 }, interval);
